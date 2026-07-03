97fa7097b88688a44daa229ca9bf88ba5a204bc7
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-07-03 12:48:36 +0300

    Connected app to the database in MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
@@ -6,6 +10,10 @@ app.get("/", (req, res) => {
   res.send("Hello from backend!. Welcome to the Products App");
 })
 
-app.listen(5000, (req, res) => {
+connectDB().then(() => {
+  app.listen(5000, (req, res) => {
   console.log('Server is running on port 5000');
 });
+})
+
+// SDzytvkCqQeVhLqQ
