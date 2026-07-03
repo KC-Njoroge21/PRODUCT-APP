f288c7cc3f105b6ca7410c17e107155d468d33d9
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-07-03 13:37:38 +0300

    Created the products POST request

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,33 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
+import Product from './model/productModel.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from backend!. Welcome to the Products App");
+app.use(express.json());
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.description) {
+    return (
+      res.status(400).json({ message: "Please provide all required fields" })
+    )
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(201).json({success: true, data: savedProduct, message: "Product created successfully"});
+  } catch (error) {
+    res.status(500).json({success: false, message: "Failed to create product"});
+    console.error(error);
+  }
+
 })
 
 connectDB().then(() => {
