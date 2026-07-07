ec2b01770ac821fae31a469902fd9ca41bbffc3b
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-07-07 12:13:52 +0300

    Installed cors and added the original frontend port

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,24 @@ import dotenv from 'dotenv';
 import Product from './model/productModel.js';
 import mongoose from 'mongoose';
 import productRoutes from './routes/productRoute.js';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
+
+app.use(cors({origin: 'http://localhost:5000'}));
 
 app.use("/api/products", productRoutes); 
 
 
 connectDB().then(() => {
-  app.listen(5000, (req, res) => {
+  app.listen(PORT, (req, res) => {
   console.log('Server is running on port 5000');
 });
 })
 
-// SDzytvkCqQeVhLqQ
