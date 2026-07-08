9121fe1846257bce567d421f100960aeaa1e3984
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-07-08 17:37:05 +0300

    Created a productData usestate for storing the information submitted on the form

--- frontend/src/pages/CreateProduct.jsx
+++ frontend/src/pages/CreateProduct.jsx
@@ -1,23 +1,34 @@
 import React from "react";
 
 const CreateProduct = () => {
+
+  const [productData, setProductData] = useState({
+    name: "",
+    price: "",
+    description: "",
+  })
+
   return (
     <section className="bg-gray-100 h-screen">
       <div className="p-4">
         <h3 className="text-xl font-bold text-center">Create Product</h3>
       </div>
+
       <form className="p-4 flex flex-col gap-3 w-1/2 mx-auto min-w-70 rounded-lg shadow-md bg-white" action="">
+
         <div className="flex flex-col gap-2">
           <label className="font-semibold" htmlFor="name">Name</label>
-          <input className="p-2 outline-0 border rounded-md" type="text" name="name" id="name" />   
+          <input className="p-2 outline-0 border rounded-md" type="text" name="name" id="name" value={productData.name} />   
         </div>
+
         <div className="flex flex-col gap-2">
           <label className="font-semibold" htmlFor="price">Price</label>
-          <input className="p-2 outline-0 border rounded-md" type="number" name="price" id="price" />   
+          <input className="p-2 outline-0 border rounded-md" type="number" name="price" id="price" value={productData.price} />   
         </div>
+        
         <div className="flex flex-col gap-2 ">
           <label className="font-semibold" htmlFor="description">Description</label>
-          <textarea className="p-2 outline-0 border rounded-md" name="description" id="description" cols="30" rows="5"></textarea> 
+          <textarea className="p-2 outline-0 border rounded-md" name="description" id="description" cols="30" rows="5" value={productData.description}></textarea> 
         </div>
       </form>
     </section>
