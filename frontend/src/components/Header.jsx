85964e4d631b4552e00a17a9b9ab8f8305ef0025
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-07-08 17:32:35 +0300

    Created and styled the CreateProduct page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className='flex justify-between items-center p-4 bg-gray-800 text-white'>
-      <h3>PRODUCT STORE</h3>
+      <h3 className="text-sm md:text-xl font-bold">PRODUCT STORE</h3>
       <div>
         <Link to="/create">
-        <button className='flex items-center gap-2 bg-blue-500 text-white p-2 rounded hover:bg-blue-600'>
-          <GoPlus size={25} />Create Product
+        <button className='flex items-center gap-2 bg-blue-500 text-white text-sm md:text-xl p-2 rounded hover:bg-blue-600'>
+          <GoPlus size={20} />Create Product
         </button>
         </Link>
         
--- frontend/src/pages/CreateProduct.jsx
+++ frontend/src/pages/CreateProduct.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const CreateProduct = () => {
   return (
-    <div>CreateProduct</div>
-  )
-}
+    <section className="bg-gray-100 h-screen">
+      <div className="p-4">
+        <h3 className="text-xl font-bold text-center">Create Product</h3>
+      </div>
+      <form className="p-4 flex flex-col gap-3 w-1/2 mx-auto min-w-70 rounded-lg shadow-md bg-white" action="">
+        <div className="flex flex-col gap-2">
+          <label className="font-semibold" htmlFor="name">Name</label>
+          <input className="p-2 outline-0 border rounded-md" type="text" name="name" id="name" />   
+        </div>
+        <div className="flex flex-col gap-2">
+          <label className="font-semibold" htmlFor="price">Price</label>
+          <input className="p-2 outline-0 border rounded-md" type="number" name="price" id="price" />   
+        </div>
+        <div className="flex flex-col gap-2 ">
+          <label className="font-semibold" htmlFor="description">Description</label>
+          <textarea className="p-2 outline-0 border rounded-md" name="description" id="description" cols="30" rows="5"></textarea> 
+        </div>
+      </form>
+    </section>
+  );
+};
 
-export default CreateProduct
+export default CreateProduct;
