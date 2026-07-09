d895ca15622934e92291f28cb7ecfc18155815c4
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-07-09 08:59:56 +0300

    Installed react hot toast and added its functionality to Products.js

--- frontend/src/store/Products.js
+++ frontend/src/store/Products.js
@@ -1,29 +1,41 @@
-import {create} from 'zustand'
+import toast from "react-hot-toast";
+import { create } from "zustand";
 
 export const useProductStore = create((set) => ({
   products: [],
   setProducts: (products) => set({ products }),
   createProduct: async (newProduct) => {
     if (!newProduct.name || !newProduct.price || !newProduct.description) {
-      return {success: false, message: "Please provide all required fields: name, price, and description."}
+      toast.error(
+        "Please provide all required fields: name, price and description.",
+      );
+      return {
+        success: false,
+        message:
+          "Please provide all required fields: name, price, and description.",
+      };
     }
 
-     try {
+    try {
       const res = await fetch("http://localhost:5000/api/products", {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(newProduct)
+        body: JSON.stringify(newProduct),
       });
       const data = await res.json();
-      set((state) => ({products: [...state.products, data.data]}))
-      return {success: true, message: "Product created successfully", data: data.data}
+      set((state) => ({ products: [...state.products, data.data] }));
+      toast.success("Product created successfully");
+      return {
+        success: true,
+        message: "Product created successfully",
+        data: data.data,
+      };
     } catch (error) {
       console.error(error);
-      return {success: false, message: "Failed to create product"}
+      toast.error("Failed to create product");
+      return { success: false, message: "Failed to create product" };
     }
-    
-    
-  }
-}))
+  },
+}));
